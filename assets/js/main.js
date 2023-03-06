2aafbda88682022a69c7bf520448024331a0921e
kwonmw12 <kwonmw12@gmail.com>
2023-03-06 17:18:13 +0900

    포트폴리오 섹션 마우스엔터 이벤트 추가, 이슈 : 파이어폭스에서 작동안함 확인. 수정중

--- assets/js/main.js
+++ assets/js/main.js
@@ -205,14 +205,17 @@
   /**
    * 포트폴리오 스와이퍼
    */
-  new Swiper('.portfolio-slider', {
+  var portfolio = new Swiper('.portfolio-slider', {
     speed: 1000,
     loop: true,
     // autoplay: {
     //   delay: 8000,
     //   disableOnInteraction: false
     // },
-
+    navigation: {
+      nextEl: "#portfolio .swiper-button-next",
+      prevEl: "#portfolio .swiper-button-prev",
+    },
     pagination: {
       el: '.portfolio .swiper-pagination',
       type: 'bullets',
@@ -220,6 +223,75 @@
     },
     paginationClickable: true,
     autoplayDisableOnInteraction: true,
+  });
+  portfolio.init();
+
+  const cursor = document.querySelector('#cursor');
+  const cursorCircle = cursor.querySelector('.cursor-circle');
+  
+  const mouse = { x: -100, y: -100 }; // mouse pointer's coordinates
+  const pos = { x: 0, y: 0 }; // cursor's coordinates
+  const speed = 0.3; // between 0 and 1
+  
+  const updateCoordinates = e => {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+  }
+  
+  window.addEventListener('mousemove', updateCoordinates);
+  
+  
+  function getAngle(diffX, diffY) {
+    return Math.atan2(diffY, diffX) * 180 / Math.PI;
+  }
+  
+  function getSqueeze(diffX, diffY) {
+    const distance = Math.sqrt(
+      Math.pow(diffX, 2) + Math.pow(diffY, 2)
+    );
+    const maxSqueeze = 0.15;
+    const accelerator = 1500;
+    return Math.min(distance / accelerator, maxSqueeze);
+  }
+  
+  
+  const updateCursor = () => {
+    const diffX = Math.round(mouse.x - pos.x);
+    const diffY = Math.round(mouse.y - pos.y);
+  
+    pos.x += diffX * speed;
+    pos.y += diffY * speed;
+  
+    const angle = getAngle(diffX, diffY);
+    const squeeze = getSqueeze(diffX, diffY);
+  
+    const scale = 'scale(' + (1 + squeeze) + ', ' + (1 - squeeze) +')';
+    const rotate = 'rotate(' + angle +'deg)';
+    const translate = 'translate3d(' + pos.x + 'px ,' + pos.y + 'px, 0)';
+  
+    cursor.style.transform = translate;
+    cursorCircle.style.transform = rotate + scale;
+  };
+  
+  function loop() {
+    updateCursor();
+    requestAnimationFrame(loop);
+  }
+  
+  requestAnimationFrame(loop);
+  
+  const cursorModifiers = document.querySelectorAll('[cursor-class]');
+  
+  cursorModifiers.forEach(curosrModifier => {
+    curosrModifier.addEventListener('mouseenter', function() {
+      const className = this.getAttribute('cursor-class');
+      cursor.classList.add(className);
+    });
+  
+    curosrModifier.addEventListener('mouseleave', function() {
+      const className = this.getAttribute('cursor-class');
+      cursor.classList.remove(className);
+    });
   });
 
 
